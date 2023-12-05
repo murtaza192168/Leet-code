eceb5255f2234388633106a9df9514ade4d16e5c
murtaza192168 <murtazacloudwork1252@gmail.com>
2023-12-05 18:46:33 +0530

    Two solutions for Problem_11 added!

--- blind75/Medium/Container-with-most-Water-Problem/Solution_11_1.java
+++ blind75/Medium/Container-with-most-Water-Problem/Solution_11_1.java
@@ -10,6 +10,15 @@ class Solution_11 {
     
 public static int maxArea(int[] height) {
 
-	
 
+// BRUTE FORCE APPROACH
+        int max_area = 0;
+        for(int i=0; i<height.length; i++){
+          for(int j=0; j<height.length; j++){
+            int area = (Math.min(height[i], height[j]) * Math.abs(i - j));
+            max_area = Math.max(max_area, area);
+          }
+        }
+        return max_area;
+   }
 }
