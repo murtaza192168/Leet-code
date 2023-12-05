5a84cf643b776c1cbd3427be6292e80cc22f811e
murtaza192168 <murtazacloudwork1252@gmail.com>
2023-12-05 18:42:13 +0530

    Two solutions for Problem_11 added

---
+++ blind75/Medium/Container-with-most-Water-Problem/Solution_11_1.java
@@ -0,0 +1,15 @@
+class Solution_11 {
+
+  public static void main(String[] args){
+     // Give user Input here
+
+     //Invoke the method of Maximum area search
+     int result = maxArea(height);
+     // Display the result
+  }
+    
+public static int maxArea(int[] height) {
+
+	
+
+}
